4951ad4f55b6343934b6c9430f3d267c76809817
Icvara <icvara.aor@gmail.com>
2026-01-27 22:41:10 +0000

    add start menu

---
+++ godot-ggj-26/Menu/landing_menu.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+
+func _on_button_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_game.tscn")
+
+
+func _on_button_settings_pressed() -> void:
+	pass # Replace with function body.
+	#get_tree().change_scene_to_file(“res://apartment stairway.tscn”)
+
+func _on_button_credit_pressed() -> void:
+	pass # Replace with function body.
+	#get_tree().change_scene_to_file(“res://apartment stairway.tscn”)
+
+func _on_button_quit_pressed() -> void:
+	get_tree().quit()
