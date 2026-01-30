a336bb0140b14ab385756eca164d625084e0e88f
Icvara <icvara.aor@gmail.com>
2026-01-30 02:17:09 +0000

    add executable

--- godot-ggj-26/Infection/infection.gd
+++ godot-ggj-26/Infection/infection.gd
@@ -2,7 +2,9 @@ extends Node3D
 
 var probability = 0.5
 var infection_state = 0
-var max_infection = 5
+var max_infection = 10
+var late_state_infection = 8
+
 
 var player: Node3D
 
@@ -11,19 +13,23 @@ func _process(delta: float) -> void:
 	if player:
 		if !player.isDead:
 			infection_state += delta
-			progress_infection()
-			if infection_state > max_infection:
-				player.Die()
+			
+			if infection_state > late_state_infection:
+				progress_infection()
+				if infection_state > max_infection:
+					player.Die()
 
 
 func infect_player(p):
 		player = p
-		var mesh := player.get_node("MeshInstance3D")
-		var mat = mesh.get_active_material(0)
-		mat.albedo_color.g = 0
+		
 
 
 func progress_infection():
 	var mesh := player.get_node("MeshInstance3D")
 	var mat = mesh.get_active_material(0)
-	mat.albedo_color.g += 0.1
+	mat = mat.duplicate()
+	mesh.set_surface_override_material(0, mat)
+	mat.albedo_color.g = 1
+	mat.albedo_color.r = 0.2
+	mat.albedo_color.b = 0.2
